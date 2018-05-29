8aedd436bfea407f2040ac3916205525688dfbb1
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-29 14:32:04 +0900

    first commit

---
+++ mail.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"github.com/nissy/bon"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"net/http"
+	"net/mail"
+	"fmt"
+	"io/ioutil"
+)
+
+func init() {
+
+	r := bon.NewRouter()
+
+	r.Post("/_ah/mail/:ToAddress", func(w http.ResponseWriter, r *http.Request) {
+		c := appengine.NewContext(r)
+		defer r.Body.Close()
+
+		m, err := mail.ReadMessage(r.Body)
+		if err != nil {
+			log.Errorf(c, "Error reading r.body: %v", err)
+			return
+		}
+
+		header := m.Header
+		log.Infof(c, "Date:", header.Get("Date"))
+		log.Infof(c, "From:", header.Get("From"))
+		log.Infof(c, "To:", header.Get("To"))
+		log.Infof(c, "Subject:", header.Get("Subject"))
+
+		body, err := ioutil.ReadAll(m.Body)
+		if err != nil {
+			log.Errorf(c, "Error reading m.body: %v", err)
+		}
+		fmt.Printf("%s", body)
+	})
+
+	http.Handle("/", r)
+}
